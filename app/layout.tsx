a4b4e633f96875b182f273476cdb558985a91f91
Svitlana <missis.chuvaeva@ukr.net>
2025-10-21 19:07:16 +0300

    add home-page(tsx & css), layout,download background-imsge

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import 'modern-normalize';
+import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
+import { Inter } from 'next/font/google';
+import Header from '../components/Header/Header';
+
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+});
+
 export const metadata: Metadata = {};
 
 export default function RootLayout({
