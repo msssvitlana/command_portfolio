12189216033c08dd4ef882fe83ac4af9e57f603c
Svitlana <missis.chuvaeva@ukr.net>
2025-10-24 20:42:26 +0300

    feat(404): add animated Matrix rain background and update page styling

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,26 +1,30 @@
-import { Metadata } from 'next';
+'use client';
+import { MatrixRainingLetters } from 'react-mdr';
 import styles from './not-found.module.css';
 import Link from 'next/link';
 
-export const metadata: Metadata = {
-  title: 'Page not found',
-  description: "The page you're looking for doesn't exist or has been moved.",
-  openGraph: {
-    title: 'Page not found',
-    description: "The page you're looking for doesn't exist or has been moved.",
-    siteName: 'WebDeveloper',
-  },
-};
 export default function NotFound() {
   return (
     <main className={styles.container}>
-      <h1 className={styles.title}>404 — Сторінку не знайдено</h1>
-      <p className={styles.description}>
-        Вибач, але сторінка, яку ти шукаєш, не існує або була видалена.
-      </p>
-      <Link className={styles.link} href="/">
-        Повернутись на головну
-      </Link>
+      <MatrixRainingLetters
+        className={styles.matrixBackground}
+        custom_class={styles.matrixBackground}
+        color="#7ea9ff"
+        backgroundColor="#0d1b2a"
+        speed={0.3}
+        density={0.6}
+        fontSize={16}
+      />
+
+      <div className={styles.content}>
+        <h1 className={styles.title}>404 — Сторінку не знайдено</h1>
+        <p className={styles.description}>
+          Вибачте, але сторінка, яку ти шукаєш, не існує або була видалена.
+        </p>
+        <Link className={styles.link} href="/">
+          Повернутись на головну
+        </Link>
+      </div>
     </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,18 @@ import Link from 'next/link';
 const Home = () => {
   return (
     <section className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.textWrapper}>
-          <h1 className={styles.title}>Уявляєш свій ідеальний сайт?</h1>
-          <p className={styles.description}>
-            <span>Цифрові рішення, що працюють.</span>
-            <span>Дизайн, який закохує.</span>
-            <span>Сайти та застосунки під ключ — швидко, стильно та без зайвого клопоту.</span>
-          </p>
-        </div>
-
-        <Link href="/about" className={css.homeBtn}>
-
-       
-
-          Відкрий можливості
-        </Link>
+      <div className={styles.textWrapper}>
+        <h1 className={styles.title}>Уявляєш свій ідеальний сайт?</h1>
+        <p className={styles.description}>
+          <span>Цифрові рішення, що працюють.</span>
+          <span>Дизайн, який закохує.</span>
+          <span>Сайти та застосунки під ключ — швидко, стильно та без зайвого клопоту.</span>
+        </p>
       </div>
+
+      <Link href="/about" className={styles.homeBtn}>
+        Відкрий можливості
+      </Link>
     </section>
   );
 };
