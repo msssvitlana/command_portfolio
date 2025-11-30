f7ffec20ac7777bc4a262a66584c17ecfb1918de
Svitlana <missis.chuvaeva@ukr.net>
2025-11-30 11:47:05 +0000

    change contacts blog styles

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -3,7 +3,23 @@ import styles from './Loader.module.css';
 const Loader = () => {
   return (
     <div className={styles.loaderWrapper}>
-      <div className={styles.spinner}></div>
+      <div className={styles.loaderBox}>
+        {/* SVG ноутбук з екраном */}
+        <svg
+          className={styles.laptop}
+          width="100"
+          height="70"
+          viewBox="0 0 100 70"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        ></svg>
+
+        <p className={styles.text}>
+          Зачекайте, будь ласка…
+          <br />
+          Йде завантаження
+        </p>
+      </div>
     </div>
   );
 };
